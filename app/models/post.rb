a8695db8ad2c459abfb63ac6a570b3293ba0d878
Allen Kim <avalmercafe02@gmail.com>
2023-03-30 15:09:48 -0400

    commit tree 5baf2fecad9df41a3995a7b89b1ff43a02f558ac

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validates :content, length: {minimum:100}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
 end
